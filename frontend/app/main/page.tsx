e3244be4b9a0c33f804851b665e91ef321e5a46d
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-08-19 19:05:42 +0700

    `Removed Popup component and its usage in Main page`

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 import AOS from "aos";
 import "aos/dist/aos.css"
 import Footer from '../components/footer/page';
-import Popup from '../components/popup/page';
 import Navbar from '../components/navbar_main/page';
 
 
 
 const Main = () => {
-    const [orderPopup, setOrderPopup] = React.useState(false);
 
     React.useEffect(() => {
         AOS.init({
@@ -26,7 +24,6 @@ const Main = () => {
         <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
             <Navbar />
             <Footer />
-            <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
         </div>
     )
 };
