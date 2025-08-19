10a18de6e271a4a16678f4f1312a2dbea1eeda6a
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-08-19 19:06:41 +0700

    `Removed Footer component from main page`

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -23,7 +23,7 @@ const Main = () => {
     return (
         <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
             <Navbar />
-            <Footer />
+            {/* <Footer /> */}
         </div>
     )
 };
