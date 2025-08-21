653e658cc734fe92ae37056c4f690671f71dd304
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-08-21 20:11:32 +0700

    `feat: updated frontend code and dependencies`

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -9,15 +9,15 @@ import bild from "../../public/images/building.svg";
 export default function Home() {
 
     return (
-        <div className="bg-white font-lato h-[1000px]">
+        <div className="bg-white font-lato">
+            {/* Section Hero */}
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between items-center    mb-10">
                     <div className="flex flex-col text-[#484848]">
                         <h1 className="font-black text-[32px]">Mau cari kos?</h1>
                         <p className="text-xl font-normal">Dapatkan infonya dan langsung sewa di Koseeker.</p>
                         <div className="flex items-center mt-5 gap-1 shadow-md border-[1px] border-[#48484819] p-2 rounded-lg">
-                            {/* icon kaca pembesar */}
-                            <IoSearch  className="text-4xl"/>
+                            <IoSearch className="text-4xl" />
                             <Search url={"/"} search={""} />
                             <button className="text-[16px] text-white cursor-pointer bg-primary font-bold px-8 py-2 rounded-sm">Cari</button>
                         </div>
@@ -27,6 +27,8 @@ export default function Home() {
                     </div>
                 </div>
             </div>
+            {/* Carousel */}
+            
         </div>
     );
 }
