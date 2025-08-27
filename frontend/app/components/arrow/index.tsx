b6ba07b027decbcdf2c829f95ef9b902c93e4e65
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-08-27 19:16:16 +0700

    `Added z-index to arrow component div`

--- frontend/app/components/arrow/index.tsx
+++ frontend/app/components/arrow/index.tsx
@@ -29,7 +29,7 @@ export function CustomArrows(props: { next: () => void; prev: () => void }) {
     const { next, prev } = props;
 
     return (
-        <div className="absolute -bottom-14 left-1/2 -translate-x-1/2 space-x-6 flex items-center gap-6">
+        <div className="absolute -bottom-14 left-1/2 -translate-x-1/2 space-x-6 flex items-center gap-6 z-10">
             <button
                 onClick={prev}
                 className="bg-white shadow-lg shadow-gray-400/50 ring-1 ring-gray-300/30 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer">
