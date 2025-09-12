25545dbf423046d1734ef1c080678f0ba744ec9f
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-09-12 22:21:07 +0700

    `Refactor KampusArea component: update button styles and layout`

--- frontend/app/components/kampus/index.tsx
+++ frontend/app/components/kampus/index.tsx
@@ -6,24 +6,20 @@ export default function KampusArea() {
         <div className="max-w-6xl mx-auto font-lato">
             <h1 className="text-2xl font-bold mb-10 text-[##323232]">Area Kos Terpopuler</h1>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-5 justify-items-center">
-                <button className="relative w-[350px] h-[100px] p-4 bg-slate-500" onClick={() => window.location.href = `/area/surabaya`}>
+                <button className="relative w-[280px] h-[120px] p-4 bg-white border-gray-400/25 border-2 hover:shadow-lg rounded-lg cursor-pointer" onClick={() => window.location.href = `/area/surabaya`}>
                     <div className="flex items-center gap-4">
-                        <img src="./images/logo-kampus/4.svg" alt="Surabaya" className="w-[60px] h-full object-cover rounded-lg" />
-                        <div className="flex flex-col items-center text-[#2e2d39]">
-                            <h1 className="text-base font-bold mb-2">
-                                UI
-                            </h1>
-                            <p className="text-base font-medium">
-                                DEPOK
-                            </p>
+                        <img src="./images/logo-kampus/4.svg" alt="Logo Kampus Unpad" className="w-[60px] h-full object-cover rounded-lg" />
+                        <div className=" flex flex-col items-start text-[#2e2d39]">
+                            <h1 className="text-base font-bold mb-1">UI</h1>
+                            <p className="text-base font-medium">Depok</p>
                         </div>
                     </div>
                 </button>
-                <button className="relative w-[280px] h-[230px] shadow-xl flex justify-center items-center rounded-lg" onClick={() => window.location.href = `#`}>
+                {/* <button className="relative w-[280px] h-[230px] shadow-xl flex justify-center items-center rounded-lg" onClick={() => window.location.href = `#`}>
                     <span className=" flex gap-2 items-center text-black text-2xl font-bold hover:underline cursor-pointer">
                         Lihat semua <BsArrowRight />
                     </span>
-                </button>
+                </button> */}
             </div>
         </div>
     )
