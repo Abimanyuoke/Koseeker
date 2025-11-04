e1a4874862391bfb89fc0b3a736442be0a347074
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-11-04 14:01:44 +0700

    `Added image and adjusted layout in area list component`

--- frontend/app/components/area_list/index.tsx
+++ frontend/app/components/area_list/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
 interface AreaData {
@@ -14,13 +15,16 @@ interface Props {
 
 export default function Arealist({ areas, linkTo }: Props) {
     return (
-        <div className="w-full">
+        <div className="w-full relative z-10">
+            <div>
+                <Image src="/images/pulau_jawa.svg" alt="Area Jawa" width={150} height={150} className="absolute top-0 right-0 w-[2000px] opacity-30 -z-10" />
+            </div>
             <h2 className="text-3xl font-bold text-gray-800 mb-10">Pilih Area Kos di Pulau Jawa</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 w-[700px] gap-6">
                 {areas.map((area) => (
                     <div
                         key={area.province}
-                        className="bg-white rounded-xl py-6">
+                        className="py-6">
                         {/* Province Header */}
                         <div className="flex items-center gap-2 mb-4 pb-3">
                             <FaMapMarkerAlt className="text-gray-500 text-xl" />
